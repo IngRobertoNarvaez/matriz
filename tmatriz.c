e5164e9a6b93d524cbc22efa14a1e80f486e5ac3
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-22 16:23:23 -0500

    Asignar matriz

--- tmatriz.c
+++ tmatriz.c
@@ -3,11 +3,17 @@ int main(int argc, char const *argv[])
 {
     int filas;
     int columnas;
-    int matriz[filas][columnas];
-    printf("Ingrese las filas : ");
+    printf("Ingrese las filas y columnas : ");
     scanf("%d", &filas);
     printf("Ingrese las Columnas : ");
     scanf("%d", &columnas);
-    printf("La matriz es de : %d", &matriz);
+    int matri[filas][columnas];
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matri[i][j] = 0;
+        }
+    }
     return 0;
 }
